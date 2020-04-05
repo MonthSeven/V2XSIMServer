73e5b10bd4f79afa75adc8dea9b18f1485de214f
Sion10032 <sion10032@hotmail.com>
2020-04-05 17:05:08 +0800

    添加模型（初版）

---
+++ V2XSIM/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class V2XsimConfig(AppConfig):
+    name = 'V2XSIM'
---
+++ V2XSIM/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Scene(models.Model):
+
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Test(models.Model):
+
+    scene = models.ForeignKey(Scene, on_delete=models.PROTECT)
+    status = models.IntegerField()
+
+class Node(models.Model):
+
+    name = models.CharField(max_length=200)
+    can_send = models.BooleanField()
+    can_recieve = models.BooleanField()
+
+    def __str__(self):
+        return "Node %s" % self.name
+
+class SceneConfig(models.Model):
+
+    scene = models.ForeignKey(Scene, on_delete=models.CASCADE)
+    node = models.ForeignKey(Node, on_delete=models.CASCADE)
+
+class V2XMessage(models.Model):
+
+    info = models.CharField(max_length=500)
+
+class MessageLog(models.Model):
+
+    message_type = models.IntegerField()
+    message = models.ForeignKey(V2XMessage, on_delete=models.PROTECT)
+    node = models.ForeignKey(Node, on_delete=models.PROTECT)
+    time = models.DateField()
